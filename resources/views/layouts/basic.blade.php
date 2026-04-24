48db2724affb4a3d57b35806e39c194ad5eba562
Jeroen Bleijenberg <jbleijenberg@ehvg.nl>
2026-04-24 12:36:00 +0200

    Added custom fonts

--- resources/views/layouts/basic.blade.php
+++ resources/views/layouts/basic.blade.php
@@ -11,7 +11,9 @@
     <link rel="shortcut icon" type="image/ico" href="{{ ($snipeSettings) && ($snipeSettings->favicon!='') ?  Storage::disk('public')->url(e($snipeSettings->favicon)) : config('app.url').'/favicon.ico' }}">
     {{-- stylesheets --}}
     <link rel="stylesheet" href="{{ url(mix('css/dist/all.css')) }}">
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <script nonce="{{ csrf_token() }}">
         window.snipeit = {
             settings: {
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -21,7 +21,9 @@
     <link rel="shortcut icon" type="image/ico"
           href="{{ ($snipeSettings) && ($snipeSettings->favicon!='') ?  Storage::disk('public')->url(e($snipeSettings->favicon)) : config('app.url').'/favicon.ico' }}">
 
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="language" content="{{ Helper::mapBackToLegacyLocale(app()->getLocale()) }}">
     <meta name="language-direction" content="{{ Helper::determineLanguageDirection() }}">
